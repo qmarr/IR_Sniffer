9a1bbe62b6222edb43b3ff058278bdcad84b4fd4
Vlad Komarenets <komarenetsv@gmail.com>
2026-04-10 22:17:20 +0300

    Implement IR transmission functionality and configure TX channel

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "freertos/queue.h"
 #include "esp_log.h"
 #include "driver/rmt_rx.h"
+#include "driver/rmt_tx.h"
 #include "driver/gpio.h"
 
 #define TAG "IR_SNIFFER"
@@ -10,6 +11,7 @@
 #define RMT_RX_GPIO GPIO_NUM_15
 #define IR_LED GPIO_NUM_16
 #define RMT_RESOLUTION_HZ 1000000 // 1us per tick
+#define IR_LENGTH 34
 
 #define MEM_BLOCK_SYMBOLS 64
 #define QUEUE_SIZE 4
@@ -66,31 +68,59 @@ void app_main(void)
     rmt_symbol_word_t raw_symbols[MEM_BLOCK_SYMBOLS];
 
     rmt_receive_config_t receive_config = {
-        .signal_range_min_ns = 1000,     
-        .signal_range_max_ns = 10000000, 
+        .signal_range_min_ns = 1000,
+        .signal_range_max_ns = 10000000,
     };
 
     ESP_ERROR_CHECK(rmt_receive(rx_channel, raw_symbols,
                                 sizeof(raw_symbols), &receive_config));
+    //
 
-
-    gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << IR_LED),
-        .mode = GPIO_MODE_OUTPUT,
+    // 5. Tx channel config
+    rmt_tx_channel_config_t tx_conf = {
+        .gpio_num = IR_LED,
+        .clk_src = RMT_CLK_SRC_DEFAULT,
+        .mem_block_symbols = MEM_BLOCK_SYMBOLS,
+        .resolution_hz = 1 * 1000 * 1000,
+        .trans_queue_depth = QUEUE_SIZE,
+        .flags.invert_out = false, // do not invert output signal
+        .flags.with_dma = false,   // do not need DMA backend
     };
 
-    gpio_config(&io_conf);
+    rmt_channel_handle_t tx_channel = NULL;
+    ESP_ERROR_CHECK(rmt_new_tx_channel(&tx_conf, &tx_channel));
 
+    // 6. carrier config
+    rmt_carrier_config_t tx_carrier_cg = {
+        .duty_cycle = 0.33,                 // duty cycle 33%
+        .frequency_hz = 36000,              // 36 KHz
+        .flags.polarity_active_low = false, // carrier should be modulated to high level
 
-    ir_symbol_t power_button[34];
+    };
+    // modulate carrier to TX channel
+    ESP_ERROR_CHECK(rmt_apply_carrier(tx_channel, &tx_carrier_cg));
+
+    // copy encoder
+    rmt_encoder_handle_t copy_encoder = NULL;
+    rmt_copy_encoder_config_t copy_encoder_config = {};
+    ESP_ERROR_CHECK(rmt_new_copy_encoder(&copy_encoder_config, &copy_encoder));
+
+    // transmit
+
+    rmt_transmit_config_t tx_trans_config = {
+        .loop_count = 0, // 0 = one loop
+    };
+    // 7. Enable tx channel
+    ESP_ERROR_CHECK(rmt_enable(tx_channel));
+
+    ir_symbol_t power_button[IR_LENGTH];
     int power_button_len = 0;
     bool captured = false;
+
+    rmt_symbol_word_t tx_symbols[IR_LENGTH];
     while (1)
     {
         rmt_rx_done_event_data_t rx_data;
-
-        gpio_set_level(IR_LED, true);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
 
         if (xQueueReceive(ir_queue, &rx_data, portMAX_DELAY))
         {
@@ -115,6 +145,25 @@ void app_main(void)
                 power_button_len = count;
                 captured = true;
                 ESP_LOGI(TAG, "Power button captured: %d symbols", power_button_len);
+
+                for (size_t i = 0; i < power_button_len; i++)
+                {
+                    tx_symbols[i].level0 = 1;
+                    tx_symbols[i].duration0 = power_button[i].duration0;
+                    tx_symbols[i].level1 = 0;
+                    tx_symbols[i].duration1 = power_button[i].duration1;
+                }
+                ESP_LOGI(TAG, "Power button copied to tx_symbols");
+
+                ESP_ERROR_CHECK(rmt_transmit(
+                    tx_channel,
+                    copy_encoder,
+                    tx_symbols,
+                    power_button_len * sizeof(rmt_symbol_word_t),
+                    &tx_trans_config));
+
+                ESP_ERROR_CHECK(rmt_tx_wait_all_done(tx_channel, portMAX_DELAY));
+                ESP_LOGI(TAG, "Transmit done");
             }
             else
             {
